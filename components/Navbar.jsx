087df0cfcc02457f5beb64c32c44265f676d0b7c
marcos <marcoscorralescastro.daw2020@riberadeltajo.es>
2023-05-27 20:35:23 +0200

    primer commit

---
+++ components/Navbar.jsx
@@ -0,0 +1,58 @@
+import Link from 'next/link'
+import React from 'react'
+import { CiUser, CiShoppingCart, CiSearch } from 'react-icons/ci'
+import { GiLargeDress } from 'react-icons/gi'
+
+const navbar = () => {
+  return (
+    <header className="flex justify-between flex-center">
+      <div className="logo">
+        <GiLargeDress size={52} />
+      </div>
+      <nav>
+        <ul className="flex gap-5">
+          <li>
+            <Link href="/shop">Shop</Link>
+          </li>
+          <li>
+            <Link href="/products">Most wanted</Link>
+          </li>
+          <li>
+            <Link href="/products">New arrival</Link>
+          </li>
+          <li>
+            <Link href="/products">Brands</Link>
+          </li>
+          {/* Add more navigation links as needed */}
+        </ul>
+      </nav>
+      <div className="flex">
+        <div class="max-w-md mx-auto">
+          <div class="relative flex items-center w-full h-12 rounded-lg focus-within:shadow-lg bg-white overflow-hidden">
+            <div class="grid place-items-center h-full w-12 text-gray-300">
+              <CiSearch size={20} />
+            </div>
+
+            <input
+              class="peer h-full w-full outline-none text-sm text-gray-700 pr-2"
+              type="text"
+              id="search"
+              placeholder="Search something.."
+            />
+          </div>
+        </div>
+
+      <div className='flex items-center'>
+        <div className="user-buttons">
+          <CiUser size={24} />
+        </div>
+        <div className="cart">
+          <CiShoppingCart size={24} />
+        </div>
+      </div>
+      </div>
+    </header>
+  );
+}
+
+export default navbar
